ce2b9e8de683515acbc40530aa785f19c28011ec
André Ligné <hello@bzf.se>
2016-07-16 17:24:38 +0200

    Remove broken test

--- tests/GridTest.elm
+++ tests/GridTest.elm
@@ -6,20 +6,15 @@ import Grid
 import Dict
 
 
+buildCell : ( Int, Int ) -> Int -> ( ( Int, Int ), { value : Int } )
+buildCell point x =
+    ( point, { value = x } )
+
+
 tests : Test
 tests =
     describe "GridTest"
-        [ describe "Grid.columns"
-            [ it "returns the columns sorted by their row value" <|
-                expect (Grid.columns (Dict.fromList [ ( ( 0, 0 ), 2 ), ( ( 0, 1 ), 2 ), ( ( 0, 2 ), 4 ), ( ( 0, 3 ), 16 ), ( ( 1, 0 ), 2 ), ( ( 1, 2 ), 4 ), ( ( 1, 3 ), 16 ), ( ( 2, 2 ), 8 ), ( ( 2, 3 ), 16 ), ( ( 3, 2 ), 2 ), ( ( 3, 3 ), 8 ) ]))
-                    toBe
-                    [ [ ( ( 0, 0 ), 2 ), ( ( 0, 1 ), 2 ), ( ( 0, 2 ), 4 ), ( ( 0, 3 ), 16 ) ]
-                    , [ ( ( 1, 0 ), 2 ), ( ( 1, 2 ), 4 ), ( ( 1, 3 ), 16 ) ]
-                    , [ ( ( 2, 2 ), 8 ), ( ( 2, 3 ), 16 ) ]
-                    , [ ( ( 3, 2 ), 2 ), ( ( 3, 3 ), 8 ) ]
-                    ]
-            ]
-        , let
+        [ let
             grid =
                 Grid.new
 
